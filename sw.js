17776696228970ab4af49fb760c4b7cee19c2ba3
olivervonderlippe-lab <oliver.vonderlippe@me.com>
2026-02-18 17:24:06 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'athletik-v9-0-0';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  if (req.method !== 'GET') return;
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    const cached = await cache.match(req, {ignoreSearch: true});
+    const fetchPromise = fetch(req).then((res) => {
+      if (res && res.status === 200 && res.type === 'basic') {
+        cache.put(req, res.clone());
+      }
+      return res;
+    }).catch(() => cached);
+
+    return cached || fetchPromise;
+  })());
+});
